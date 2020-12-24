e95849821bea3984d07cd60d3d76b222a65e22da
Pomax <pomax@nihongoresources.com>
2020-12-24 13:42:14 -0800

    Update fix.py

--- fix.py
+++ fix.py
@@ -3,25 +3,18 @@ import os
 midi_files = [f for f in os.listdir('.') if os.path.isfile(f) and '.MID' in f]
 
 for filename in midi_files:
-    # Open the file in binary read/write mode:
     file = open(filename, "rb+")
-
-    # As single-track MIDI files, we know that the track length is
-    # equal to the file size, minus the header size up to and
-    # including the length value, which is 22 bytes:
     file_size = os.path.getsize(filename)
     track_length = file_size - 22
 
-    # With that done, we can read in the file as a byte array:
-    data = bytearray(file.read())
+    field_value = bytearray([
+        (track_length & 0xFF000000) >> 24,
+        (track_length & 0x00FF0000) >> 16,
+        (track_length & 0x0000FF00) >> 8,
+        (track_length & 0x000000FF),
+    ])
 
-    # And update the track length bytes to the correct value:
-    data[18] = (track_length & 0xFF000000) >> 24
-    data[19] = (track_length & 0x00FF0000) >> 16
-    data[20] = (track_length & 0x0000FF00) >> 8
-    data[21] = (track_length & 0x000000FF)
-
-    # Then we write the update file back to disk:
-    file.seek(0)
-    file.write(data)
-    print(f"Updated {filename} track length to {track_length} bytes")
+    file.seek(18)
+    file.write(file_value)
+    file.close()
+    print(f"Updated {filename} track length to {track_length}")
